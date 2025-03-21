c45b3b8b4dd6fc13726484ba2c289fe6e449bae9
MidwestGames <82992794+MidwestGames@users.noreply.github.com>
2025-03-21 10:52:17 -0600

    Submitting changes for work on another computer

--- DiscreteEventSimulation/objects/Entity.cs
+++ DiscreteEventSimulation/objects/Entity.cs
@@ -1,4 +1,6 @@
-﻿namespace DiscreteEventSimulation.objects.interfaces
+﻿using System.Collections.Generic;
+
+namespace DiscreteEventSimulation.objects.interfaces
 {
     public class Entity
     {
@@ -7,14 +9,25 @@
         // Required Parameters taken from imported data
         #region Required Parameters
 
+        protected string ceid;
+        protected float availability;
+        protected sbyte inventory;
+        protected List<string> entities;
+
         #endregion
 
         // Calculated Parameters taken from imported data
+
         #region Calculated Parameters
+
+        // Index for statistical metrics (From Actual Factory Data):
+        // 0: Min | 1: 25%tile | 2: Mean | 3: Median | 4: 75%tile | 5: 90%tile | 6: Max | 7: Standard Deviation | 8: Variance
+        protected List<float> availabilitySummary;
 
         #endregion
 
         //Simulated values generated from factory controller
+
         #region Simulated Values
 
         #endregion
